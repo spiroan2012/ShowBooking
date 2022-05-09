19a7e7a0c72313fb96c60d686869232b837e53bc
Andreas Spiropoulos <spiroan2000@windowslive.com>
2022-05-09 22:25:26 +0300

    Multiple fixes closes #5, #4, #2

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -61,7 +61,7 @@ namespace API.Controllers
             var result = await _signInManager
                 .CheckPasswordSignInAsync(user, loginDto.Password, false);
 
-            if (!result.Succeeded) return Unauthorized();
+            if (!result.Succeeded) return BadRequest("Λάθος κωδικός πρόσβασης");
 
             return new UserDto
             {
